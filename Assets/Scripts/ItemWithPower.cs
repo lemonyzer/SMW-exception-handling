5d0f5fc034a297ea6f6f5cd7fe5cccc8337bc61b
Lemon Nyzer <lemonyzer@gmail.com>
2015-03-11 23:50:16 +0100

    "SuperMarioWars 2015.03.11_1_Unity5_newUI_aLotOfBugs.rar"

---
+++ Assets/Scripts/ItemWithPower.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ItemWithPower : Item {
+
+	public abstract override void Collecting(GameObject itemGO, PlatformCharacter collector);
+	public abstract override void Collected(PlatformCharacter collector, NetworkMessageInfo info);
+
+}
